cf4577aeea0f99f9fecd3391a9e67be237a1050e
Chuck Bryan <cbryan@marathonus.com>
2025-05-13 15:20:11 -0400

    fix: refactor version update logic in update-version.ps1 for clarity

--- scripts/update-version.ps1
+++ scripts/update-version.ps1
@@ -17,23 +17,23 @@ try {
     if (Test-Path $dirBuildPropsPath) {
         # Update existing Directory.Build.props
         $xml = [xml](Get-Content $dirBuildPropsPath)
-        $versionElement = $xml.Project.PropertyGroup.Version
+        $propertyGroup = $xml.Project.PropertyGroup
         
-        if ($null -eq $versionElement) {
-            $propertyGroup = $xml.Project.PropertyGroup
-            if ($null -eq $propertyGroup) {
-                $propertyGroup = $xml.CreateElement("PropertyGroup")
-                $xml.Project.AppendChild($propertyGroup)
-            } elseif ($propertyGroup -is [array]) {
-                $propertyGroup = $propertyGroup[0]
-            }
-            
-            $versionElement = $xml.CreateElement("Version")
-            $versionElement.InnerText = $Version
-            $propertyGroup.AppendChild($versionElement)
-        } else {
-            $versionElement.InnerText = $Version
+        if ($null -eq $propertyGroup) {
+            $propertyGroup = $xml.CreateElement("PropertyGroup")
+            $xml.Project.AppendChild($propertyGroup)
+        } elseif ($propertyGroup -is [array]) {
+            $propertyGroup = $propertyGroup[0]
         }
+        
+        $versionElement = $propertyGroup.SelectSingleNode("Version")
+        if ($null -eq $versionElement) {
+            $versionElement = $xml.CreateElement("Version")
+            $propertyGroup.AppendChild($versionElement)
+        }
+        
+        # Safely update or set the InnerText
+        $versionElement.InnerText = $Version
     } else {
         # Create new Directory.Build.props
         $xml = [xml]"<Project><PropertyGroup><Version>$Version</Version></PropertyGroup></Project>"
